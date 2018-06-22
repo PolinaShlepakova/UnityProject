1415b66fdfd7fc280f8e390498fb896021882ed6
Polina Shlepakova <polinashlepakova@gmail.com>
2018-06-22 17:47:36 +0300

    Fruits label added

---
+++ Assets/Scripts/UI/FruitsLabel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FruitsLabel : MonoBehaviour {
+
+	public int NFruits = 9;
+
+	private string _trailingText;
+
+	void Start() {
+		_trailingText = "/" + NFruits;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GetComponentInChildren<Text>().text = LevelController.Current.Fruits + _trailingText;
+	}
+}
