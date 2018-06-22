719ea51193fc8027bb662124d0224a633ad9c982
Polina Shlepakova <polinashlepakova@gmail.com>
2018-06-22 15:40:51 +0300

    Money label added

---
+++ Assets/Scripts/UI/MoneyLabel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyLabel : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponentInChildren<Text>().text = LevelController.Current.Coins.ToString("0000");
+	}
+}
